7b08d92c27c075edd73c84e198f8caf6ba2ff8a8
Peter Kriens <Peter.Kriens@aqute.biz>
2021-02-05 09:33:48 +0100

    init

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# expects REPOSITORY_USERNAME & REPOSITORY_PASSWORD to be set
+# and ../build contains the resources
+
+echo "Downloading bnd"
+curl https://repo1.maven.org/maven2/biz/aQute/bnd/biz.aQute.bnd/5.2.0/biz.aQute.bnd-5.2.0.jar >bnd.jar
+
+echo Build bundle
+java -jar bnd.jar build
+
+ls com.heilaiq.portal.ui.webresource/generated/com.heilaiq.portal.ui.webresource.jar
+
+echo Release
+java -jar bnd.jar release -w
+
